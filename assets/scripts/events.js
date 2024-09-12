ff3df2f4717b286f9b19a8b573128aabcf16cfca
kaizz04 <kaveesh@marketpushapps.com>
2024-09-12 22:42:46 +0530

    Triggering DOM element programmatically

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -49,7 +49,12 @@ const list = document.querySelector('ul');
 // });
 
 list.addEventListener('click',event =>{
-    event.target.classList.toggle('highlight');
+    // event.target.classList.toggle('highlight');
+    event.target.closest('li').classList.toggle('highlight');
+    //form.submit();
+    button.click();
 });
+
+
 
 
